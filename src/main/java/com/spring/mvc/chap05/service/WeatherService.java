1ce06f9a3eea5bb27a19e22ab6d69219758b85ab
wonjinseop <wonjinseop7@naver.com>
2024-04-08 17:29:23 +0900

    #38. 공공데이터 API 사용 방법(기상청 단기예보)

--- src/main/java/com/spring/mvc/chap05/service/WeatherService.java
+++ src/main/java/com/spring/mvc/chap05/service/WeatherService.java
@@ -9,7 +9,6 @@ import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
