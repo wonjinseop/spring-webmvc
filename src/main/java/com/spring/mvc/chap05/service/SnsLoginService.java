6c58b2a10fdd661181d8a3db6a128b34821debfd
wonjinseop <wonjinseop7@naver.com>
2024-04-09 13:23:40 +0900

    #39. 카카오 로그인 신청 및 인가코드 받기

---
+++ src/main/java/com/spring/mvc/chap05/service/SnsLoginService.java
@@ -0,0 +1,18 @@
+package com.spring.mvc.chap05.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@Slf4j
+public class SnsLoginService {
+    
+    
+    public void kakoLogin(Map<String, String> params) {
+    
+    
+    
+    }
+}
